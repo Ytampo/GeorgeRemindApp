b0ac9c0f4ea90af75232fd7c8cc2f1220ef2cba1
tanimoto <98684774+Ytampo@users.noreply.github.com>
2024-10-07 14:44:32 +0900

    add: cssとjsの追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+
             <!-- ノッチ領域の余白 -->
             <div class="notch-bar"></div>
 
